a5f82baccf43a57777111d53b5a9240ab7d86554
Rasmus <viprocoding@gmail.com>
2014-12-02 18:20:20 +0100

    Dev A is still the best

--- TestProject/test.c
+++ TestProject/test.c
@@ -2,10 +2,9 @@
 
 int main(int argc, char *argv[])
 {
-	// Developer A ( LOL -- Developer B is best)
-	printf("Developer B is the best\n");
+	// Developer A
+	printf("Developer A is the best\n");
 
-	printf("B was here\n");
 
 	return 0;
 }
