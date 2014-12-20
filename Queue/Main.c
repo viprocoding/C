1d79c20a7892658a22e5d8ef17c74df66579ff96
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2014-12-20 14:56:46 +0100

    Simple string test

--- Queue/Main.c
+++ Queue/Main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include "Queue.h"
+#include <stdlib.h>
+
+void stringFree(void* elem)
+{
+	free(*(char**) elem);
+}
 
 int main(int argc, char *argv[])
 {
 	queue_t q;
+	char *s = "ELITE";
+	char *t;
 
-	queueInit(&q, sizeof(int));
+	queueInit(&q, sizeof(char*), stringFree);
 
 	for (int i = 0; i < 10; i++)
-		if (queueEnqueue(&q, &i) == ERR_REALLOC) {
+		if (queueEnqueue(&q, &s) == ERR_REALLOC) {
 			fprintf(stderr, "Error: Could not realloc during grow-procces.\n");
 			break;
 		}
 	
-	for (int i = 0, j; i < 11; i++) {
-		if (queueDequeue(&q, &j) == ERR_EMPTY)
+	for (int i = 0; i < 11; i++) {
+		if (queueDequeue(&q, &t) == ERR_EMPTY)
 			fprintf(stderr, "Error: Empty stack at itteration %d\n", i);
 		else
-			printf("Dequeue result: %d\n", j);
+			printf("Dequeue result: %s\n", t);
 	}
 
 	queueDispose(&q);
