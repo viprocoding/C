c98575c38a4f386f3e376f2dc0d89dd685bcdb2c
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2014-12-20 16:04:27 +0100

    More doc. + retab

--- Queue/Queue.c
+++ Queue/Queue.c
@@ -5,99 +5,110 @@
  * 2014-12-19
  *
  * A generic and dynamic array implementation of a queue.
- *
  */
 
+// Libraries, headers
 #include "Queue.h"
-#include <stdlib.h>		// malloc, free
-#include <string.h>		// memcpy
+#include <stdlib.h>     // malloc, free
+#include <string.h>     // memcpy
 
+/** queueGrow:
+ *      Static function which is used to increase the size of a queue.
+ *
+ *  @param q        Pointer to a queue that should grow.
+ *
+ *  @return         ERR_MALLOC  Heap memory could not be allocated (grow
+ *                              cancelled).
+ *                  SUCCESS     Queue grew by a factor of 2.
+ */
 static int queueGrow(queue_t* q)
 {
-	void *tmp;
+    void *tmp;
 
-	// Grow by factory 2
-	if ((tmp = malloc(q->capacity * 2 * q->elemSize)) == NULL)
-		return ERR_MALLOC;
-	
-	// Copy old queue
-	for (int i = 0; i < q->capacity; i++)
-		queueDequeue(q, (char*) tmp + i * q->elemSize);
+    // Attempt to grow by a factor of 2
+    if ((tmp = malloc(q->capacity * 2 * q->elemSize)) == NULL)
+        return ERR_MALLOC;
+    
+    // Copy old queue
+    for (int i = 0; i < q->capacity; i++)
+        queueDequeue(q, (char*) tmp + i * q->elemSize);
 
-	// Clean up -- compleete growing process
-	free(q->elems);
-	q->elems       = tmp;
-	q->front       = 0;
-	q->currentSize = q->capacity;
-	q->capacity  <<= 1;
+    // Clean up -- compleete growing process
+    free(q->elems);
+    q->elems       = tmp;
+    q->front       = 0;             // front is always @0 after a grow process
+    q->currentSize = q->capacity;   // currentSize would be 0 if this is not set
+    q->capacity  <<= 1;
 
-	return SUCCESS;
+    return SUCCESS;
 }
 
 int queueInit(queue_t* q, int elemSize, void (*freeFnk)(void*))
 {
-	if (elemSize < 1)
-		return ERR_INVALID_ARGUMENT;
-	
-	// initialize queue	
-	q->capacity    = INIT_CAPACITY;
-	q->currentSize = 0;
-	q->front       = 0;
-	q->elemSize    = elemSize;
-	q->elems       = malloc(q->capacity * q->elemSize);
-	q->freeFnk     = freeFnk;
+    if (elemSize < 1)
+        return ERR_INVALID_ARGUMENT;
+    
+    // initialize queue 
+    q->capacity    = INIT_CAPACITY;
+    q->currentSize = 0;
+    q->front       = 0;
+    q->elemSize    = elemSize;
+    q->elems       = malloc(q->capacity * q->elemSize);
+    q->freeFnk     = freeFnk;
 
-	return q->elems ? SUCCESS : ERR_MALLOC;
+    return q->elems ? SUCCESS : ERR_MALLOC;
 }
 
 void queueDispose(queue_t* q)
 {
-	void *tmp = NULL;
+    void *tmp = NULL;
 
-	if (q->freeFnk)
-		for (int i = 0, n = q->currentSize; i < n; i++) {
-			queueDequeue(q, tmp);	// here q is never empty -- dont error check
-			q->freeFnk(tmp);
-		}
+    // free elements if there's a free function
+    if (q->freeFnk)
+        for (int i = 0, n = q->currentSize; i < n; i++) {
+            queueDequeue(q, tmp);   // here q is never empty -- dont error check
+            q->freeFnk(tmp);
+        }
 
-	free(q->elems);
-	q->capacity = q->currentSize = q->front = q->elemSize = -1;
-	q->elems = NULL;
+    // clean up
+    free(q->elems);
+    q->capacity = q->currentSize = q->front = q->elemSize = -1;
+    q->elems = NULL;
 }
 
 int queueEnqueue(queue_t* q, void* elem)
 {
-	if (queueIsFull(q))
-		if (queueGrow(q) == ERR_REALLOC)
-			return ERR_MALLOC;
-	
-	// enqueue
-	int index = (q->front + q->currentSize) % q->capacity;
-	memcpy((char*) q->elems + index * q->elemSize, elem, q->elemSize);
+    if (queueIsFull(q))
+        if (queueGrow(q) == ERR_REALLOC)
+            return ERR_MALLOC;
+    
+    // enqueue
+    int index = (q->front + q->currentSize) % q->capacity;
+    memcpy((char*) q->elems + index * q->elemSize, elem, q->elemSize);
 
-	++q->currentSize;
-	return SUCCESS;
+    ++q->currentSize;
+    return SUCCESS;
 }
 
 int queueDequeue(queue_t* q, void* elem)
 {
-	if (queueIsEmpty(q))
-		return ERR_EMPTY;
-	
-	// dequeue
-	memcpy(elem, (char*) q->elems + q->front * q->elemSize, q->elemSize);
-	q->front = (q->front + 1) % q->capacity;
+    if (queueIsEmpty(q))
+        return ERR_EMPTY;
+    
+    // dequeue
+    memcpy(elem, (char*) q->elems + q->front * q->elemSize, q->elemSize);
+    q->front = (q->front + 1) % q->capacity;
 
-	--q->currentSize;
-	return SUCCESS;
+    --q->currentSize;
+    return SUCCESS;
 }
 
 int queueIsFull(queue_t* q)
 {
-	return q->currentSize == q->capacity;
+    return q->currentSize == q->capacity;
 }
 
 int queueIsEmpty(queue_t* q)
 {
-	return q->currentSize == 0;
+    return q->currentSize == 0;
 }
--- Queue/Queue.h
+++ Queue/Queue.h
@@ -2,10 +2,9 @@
  * Queue.h
  *
  * Rasmus Östersjö
- * 2014-12-XX
+ * 2014-12-19
  *
  * A generic and dynamic array implementation of a queue.
- *
  */
 
 #ifndef __QUEUE_H__
@@ -23,78 +22,77 @@
 // Queue data type
 typedef struct Queue
 {
-	int   capacity;
-	int   currentSize;
-	int   front;
-	int   elemSize;
-	void* elems;
-	
-	void (*freeFnk)(void* elem);
+    int   capacity;
+    int   currentSize;
+    int   front;
+    int   elemSize;
+    void* elems;
+    
+    void (*freeFnk)(void* elem);
 } queue_t;
 
 /** queueInit
- *		Initializes a new queue with capacity INIT_CAPACITY and a current size
- *		of 0.
+ *      Initializes a new queue with capacity INIT_CAPACITY and a current size
+ *      of 0.
  *
- *  @param q		A pointer to a queue that is to be initialized.
- *  @param elemSize	Size of each element in the queue (bytes).
- *	@param freeFnk  A pointer to a function that knows how to free each element
- *					in the queue, NULL if elements does not need to be freed.
- *	
- *	@return			ERR_MALLOC	Heap memory could not be allocated.
- *					SUCCESS
+ *  @param q        A pointer to a queue that is to be initialized.
+ *  @param elemSize Size of each element in the queue (bytes).
+ *  @param freeFnk  A pointer to a function that knows how to free each element
+ *                  in the queue, NULL if elements does not need to be freed.
+ *  
+ *  @return         ERR_MALLOC  Heap memory could not be allocated.
+ *                  SUCCESS
  */
 int queueInit(queue_t* q, int elemSize, void (*freeFnk)(void* elem));
 
 /** queueDispose
- *		Disposes a queue.
+ *      Disposes a queue.
  *
- *	@param q		A pointer to a queue that is to be disposed.
+ *  @param q        A pointer to a queue that is to be disposed.
  */
 void queueDispose(queue_t* q);
 
 /** queueEnqueue
- * 		Enqueues an elemenet to the specified queue.
+ *      Enqueues an elemenet to the specified queue.
  *
- *	@param q		A pointer to a queue.
- *	@param elem		A pointer to an element that is to be enqueued.
+ *  @param q        A pointer to a queue.
+ *  @param elem     A pointer to an element that is to be enqueued.
  *
- *	@return			ERR_MALLOC	Heap memory could not be allocated during grow-
- *								proccess.
- *					SUCCESS
+ *  @return         ERR_MALLOC  Heap memory could not be allocated during grow-
+ *                              proccess.
+ *                  SUCCESS
  */
 int queueEnqueue(queue_t* q, void* elem);
 
 /** queueDequeue
- * 		Dequeues am element from the specified queue.
+ *      Dequeues am element from the specified queue.
  *
- *	@param q		A pointer to a queue.
- *	@param elem		Dequeue result is written to this addres.
+ *  @param q        A pointer to a queue.
+ *  @param elem     Dequeue result is written to this addres.
  *
- *	@return			ERR_EMPTY	Queue was empty (dequeue canceled).
- *					SUCCESS
+ *  @return         ERR_EMPTY   Queue was empty (dequeue canceled).
+ *                  SUCCESS
  */
 int queueDequeue(queue_t* q, void* elem);
 
 /** queueIsFull
- *		Checks if the specified queue is full.
+ *      Checks if the specified queue is full.
  *
- * 	@param q		A pointer to a queue.
+ *  @param q        A pointer to a queue.
  *
- * 	@return			Non-zero	Queue is full.
- *					Zero		Queue is not full.
+ *  @return         Non-zero    Queue is full.
+ *                  Zero        Queue is not full.
  */
 int queueIsFull(queue_t* q);
 
 /** queueIsEmpty
- *		Checks if the specified queue is empty.
+ *      Checks if the specified queue is empty.
  *
- * 	@param q		A pointer to a queue.
+ *  @param q        A pointer to a queue.
  *
- * 	@return			Non-zero	Queue is empty.
- *					Zero		Queue is not empty.
+ *  @return         Non-zero    Queue is empty.
+ *                  Zero        Queue is not empty.
  */
 int queueIsEmpty(queue_t* q);
 
-
-#endif
+#endif  // __QUEUE_H__
