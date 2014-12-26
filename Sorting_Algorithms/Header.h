cd0bcead43cb834abd3d5ed2a625b7295102eaab
Rasmus Östersjö <viprocoding@gmail.com>
2014-12-26 23:44:59 +0100

    Added int version of Qsort

---
+++ Sorting_Algorithms/Header.h
@@ -0,0 +1,12 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+/** Sort an array in increasing order using quick sort.
+ *
+ * @param a     Pointer to an array that is to be sorted.
+ * @param l     Index of the left most element.
+ * @param r     Index of the last element.
+ */
+void quickSort(int *a, int l, int r);
+
+#endif // __HEADER_H__
